dbad10e2289fa34d0b444a56e810d9e167907d5e
Erwin Atuli <erwinatuli@gmail.com>
2011-04-07 01:01:23 +0800

    refactoring/renaming member vars

--- irc.py
+++ irc.py
@@ -7,14 +7,14 @@ from plugin import Plugin, IrcConfig
 
 class PYTHONIRC:
 
-    __IrcServer = ""
-    __IrcPort = "6667"
-    __IrcNick = ""
-    __IrcRoom = ""
-    __socket = None
-    __isConnected = False
-    __isAuthenticated = False
-    __plugins = []
+    IrcServer = ""
+    IrcPort = "6667"
+    IrcNick = ""
+    IrcRoom = ""
+    socket = None
+    isConnected = False
+    isAuthenticated = False
+    plugins = []
 
     # marks whether we can skip initialization
     # eg setting server/nick/channel
@@ -25,19 +25,19 @@ class PYTHONIRC:
 
     def __init__(self):
         #load the plugins
-        self.__plugins = [IrcConfig()]
+        self.plugins = [IrcConfig()]
         self.__main()
 
     def __main(self):
         self.notifyPlugins("beforeInit");
         if self.doInit :
-            self.__IrcServer = self.__getUserInput("Please Enter Irc Server Address")
+            self.IrcServer = self.__getUserInput("Please Enter Irc Server Address")
 
-            if self.__getUserInput("Do you want to change the Irc Port? The default port is " + self.__IrcPort + ". Enter y to change") == "y":
-                self.__IrcPort = self.__getUserInput("Please enter port number", "6667")
+            if self.__getUserInput("Do you want to change the Irc Port? The default port is " + self.IrcPort + ". Enter y to change") == "y":
+                self.IrcPort = self.__getUserInput("Please enter port number", "6667")
 
-            self.__IrcNick = self.__getUserInput("Please enter Irc nick")
-            self.__IrcRoom = self.__getUserInput("Please enter Irc channel")
+            self.IrcNick = self.__getUserInput("Please enter Irc nick")
+            self.IrcRoom = self.__getUserInput("Please enter Irc channel")
         self.__connect()
 
     def __getUserInput(self,msg, default=""):
@@ -46,11 +46,11 @@ class PYTHONIRC:
             return line.strip()
         return default
     def __connect(self):
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            self.__socket.connect((self.__IrcServer,int(self.__IrcPort)))
-            self.__isConnected = True
+            self.socket.connect((self.IrcServer,int(self.IrcPort)))
+            self.isConnected = True
             self.__receiveMessages()
             
         except (socket.gaierror,socket.error) as err:
@@ -61,58 +61,58 @@ class PYTHONIRC:
 
 
     def __receiveMessages(self):
-        while self.__isConnected == True:
-            buffer = self.__socket.recv(1024)
+        while self.isConnected == True:
+            buffer = self.socket.recv(1024)
             buffer = buffer.decode("utf-8")
 
             if buffer == "":
-                self.__isConnected = False
-                self.__isAuthenticated = False
+                self.isConnected = False
+                self.isAuthenticated = False
 
             else:
                 print(buffer)
 
-                if re.search("Checking Ident",buffer) and not self.__isAuthenticated:
-                    self.__sendMessage("NICK " + self.__IrcNick + "\r\n")
-                    self.__sendMessage("USER " + self.__IrcNick + " \"" + self.__IrcNick + ".com\" \"" + self.__IrcServer + "\" :" + self.__IrcNick + " robot\r\n")
+                if re.search("Checking Ident",buffer) and not self.isAuthenticated:
+                    self.__sendMessage("NICK " + self.IrcNick + "\r\n")
+                    self.__sendMessage("USER " + self.IrcNick + " \"" + self.IrcNick + ".com\" \"" + self.IrcServer + "\" :" + self.IrcNick + " robot\r\n")
 
-                elif re.search("Nickname is already in use",buffer) and not self.__isAuthenticated:
-                    self.__IrcNick = self.__getUserInput("Please enter new Irc nick")
-                    self.__sendMessage("NICK " + self.__IrcNick + "\r\n")
-                    self.__sendMessage("USER " + self.__IrcNick + " \"" + self.__IrcNick + ".com\" \"" + self.__IrcServer + "\" :" + self.__IrcNick + " robot\r\n")
-                elif re.search("Erroneous Nickname",buffer) and not self.__isAuthenticated:
-                    self.__IrcNick = self.__getUserInput("Please enter new Irc nick")
-                    self.__sendMessage("NICK " + self.__IrcNick + "\r\n")
-                    self.__sendMessage("USER " + self.__IrcNick + " \"" + self.__IrcNick + ".com\" \"" + self.__IrcServer + "\" :" + self.__IrcNick + " robot\r\n")                    
-                elif re.search("This nickname is registered",buffer) and not self.__isAuthenticated:
-                    self.__IrcNick = self.__getUserInput("Please enter new Irc nick")
-                    self.__sendMessage("NICK " + self.__IrcNick + "\r\n")
-                    self.__sendMessage("USER " + self.__IrcNick + " \"" + self.__IrcNick + ".com\" \"" + self.__IrcServer + "\" :" + self.__IrcNick + " robot\r\n")
+                elif re.search("Nickname is already in use",buffer) and not self.isAuthenticated:
+                    self.IrcNick = self.__getUserInput("Please enter new Irc nick")
+                    self.__sendMessage("NICK " + self.IrcNick + "\r\n")
+                    self.__sendMessage("USER " + self.IrcNick + " \"" + self.IrcNick + ".com\" \"" + self.IrcServer + "\" :" + self.IrcNick + " robot\r\n")
+                elif re.search("Erroneous Nickname",buffer) and not self.isAuthenticated:
+                    self.IrcNick = self.__getUserInput("Please enter new Irc nick")
+                    self.__sendMessage("NICK " + self.IrcNick + "\r\n")
+                    self.__sendMessage("USER " + self.IrcNick + " \"" + self.IrcNick + ".com\" \"" + self.IrcServer + "\" :" + self.IrcNick + " robot\r\n")                    
+                elif re.search("This nickname is registered",buffer) and not self.isAuthenticated:
+                    self.IrcNick = self.__getUserInput("Please enter new Irc nick")
+                    self.__sendMessage("NICK " + self.IrcNick + "\r\n")
+                    self.__sendMessage("USER " + self.IrcNick + " \"" + self.IrcNick + ".com\" \"" + self.IrcServer + "\" :" + self.IrcNick + " robot\r\n")
 
-                elif re.search("End of /MOTD command",buffer) and not self.__isAuthenticated:
-                    self.__isAuthenticated = True
-                    self.__sendMessage("JOIN #" + self.__IrcRoom + "\r\n")
+                elif re.search("End of /MOTD command",buffer) and not self.isAuthenticated:
+                    self.isAuthenticated = True
+                    self.__sendMessage("JOIN #" + self.IrcRoom + "\r\n")
 
                 elif re.search("PING :",buffer):
                     self.__sendMessage(buffer.replace("PING","PONG"))
 
-                elif re.search("PRIVMSG #" + self.__IrcRoom + " :",buffer):
+                elif re.search("PRIVMSG #" + self.IrcRoom + " :",buffer):
 
                     #handle room message here
                     pass
 
-                elif re.search("JOIN :#" + self.__IrcRoom,buffer):
+                elif re.search("JOIN :#" + self.IrcRoom,buffer):
                     #greet new user
 
                     nick = self.__extractNick(buffer)
                     
-                    if self.__IrcNick != nick:
-                        self.__sendMessage("PRIVMSG #" + self.__IrcRoom + " :Magandang " + self.__getMeridiem() + " sa iyo " + nick + "\r\n")
+                    if self.IrcNick != nick:
+                        self.__sendMessage("PRIVMSG #" + self.IrcRoom + " :Magandang " + self.__getMeridiem() + " sa iyo " + nick + "\r\n")
                         
 
 
     def __sendMessage(self,msg):
-        self.__socket.send(msg.encode())
+        self.socket.send(msg.encode())
     
     def __extractNick(self,str):
 
@@ -148,6 +148,6 @@ class PYTHONIRC:
         elif nowTime.tm_hour >= 18 and nowTime.tm_hour <= 23:
             return "gabi"
     def notifyPlugins (self, event):
-        for p in self.__plugins:
+        for p in self.plugins:
             p.beforeInit(self)
 run = PYTHONIRC()
