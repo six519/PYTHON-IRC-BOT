2a1864b3f4136f9e42d0055e58a2b2c96ac3a192
Erwin Atuli <erwinatuli@gmail.com>
2011-04-09 01:31:50 +0800

    removing curls.h (not needed anymore)

--- curl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cmd1="`sed -e s/user=.*/user=$1/ curl.conf > curl.conf.tmp`"
-cmd2="`sed -e s/passwrd=.*/passwrd=$2/ curl.conf.tmp > curl.conf.tmp1`"
-
-curl -K  curl.conf.tmp1 "http://www.phpugph.com/talk/index.php?action=login2"
-grep URL curl.headers | cut -f 3 -d " " | sed s/URL=//g | xargs curl -K curl.conf.tmp1 > mk
-#the last sed is a hack on a mac
-grep topicseen mk  | html2text | sed s/.//g
-rm mk curl.headers curl.cookie.jar curl.conf.tmp*
